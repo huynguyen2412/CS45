d65346a78d9bb750dbb1bf4234526e510c36ea8b
Huy Nguyen <huynguyen2412@gmail.com>
2018-12-06 02:28:20 -0800

    working on unary helper

--- final/test.cpp
+++ final/test.cpp
@@ -3,10 +3,11 @@
 
 using namespace std;
 
-void reduceToUnary(string &input);
+void reduceSign(string &input);
 bool checkPrevious(char input);
-void unaryHelper(string &input,unsigned long long &firstPos,unsigned long long firstSize,string &second);
+void minusPlusHelper(string &input,unsigned long long &firstPos);
 bool getInput(string &line);
+void unaryHelper(string &exp);
 
 int main(int argc, char const *argv[])
 {
@@ -14,7 +15,9 @@ int main(int argc, char const *argv[])
     while(getInput(line))
     {
         cout <<line<<endl;
-        reduceToUnary(line);
+        reduceSign(line);
+        cout << line<<endl;
+        unaryHelper(line);
         cout << line<<endl;
     }
     return 0;
@@ -30,19 +33,17 @@ bool getInput(string &line)
     return line != "";                            //see if the line was empty
 }
 
-
-void reduceToUnary(string &input)
+//call helper to reduce a streak of '-' or '+'
+void reduceSign(string &input)
 {
     int count = 0;
-    unsigned long long i =0;
+    unsigned long long pos =0;
     unsigned long long firstSize = input.size();        //the original length
-    input += '#';
-    string second = "";
-    while(input[i] != '#')
+    while(input[pos])
     {       
-        if(input[i] == '-' || input[i] == '+')
-            unaryHelper(input,i,firstSize,second);
-        else i++;
+        if(input[pos] == '-' || input[pos] == '+')
+            minusPlusHelper(input,pos);
+        else pos++;
     }
 }
 
@@ -53,18 +54,47 @@ bool checkPrevious(char input)
     return (temp.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789)! "));
 }
 
-void unaryHelper(string &input,unsigned long long &pos,unsigned long long firstSize,string &second)
+
+//reduce a streak of '-' or '+' to a single operator
+void minusPlusHelper(string &input,unsigned long long &pos)
 {
     int sign=0;
     unsigned long long firstPos = pos;
+    unsigned long long current;             //the position of pos 
+                                            //when pointer hit number or operator 
+                                            //is not unary or + - operator 
     //the beginning operator
     while(input[pos] == '-' || input[pos] == '+')
         if(input[pos++] == '-')
             sign++;
-
     //positive sign
     (sign%2 == 0) ?input[pos-1] = '+': input[pos-1] = '-'; //change sign of last operator
-    //remove extra operators after parsing   
-    input.erase(firstPos,pos-firstPos-1);
-    pos=0;
+     //reduce extra operators after parsing   
+    input.erase(firstPos,current=pos-firstPos-1);
+    //move pointer to new pos
+    pos = pos-current;
+}
+
+
+//Distinguish between unary (+,-) or a binary sign(-,+)
+//remove unary operator if it's +, replace it by "#" if there is unary(-)
+//otherwise,binary sign doesn't need to change.  
+void unaryHelper(string &exp)
+{
+    unsigned long long i=0;     //index of a char in string exp
+    char check ;          
+    while(exp[i])
+    {
+        //unary case 
+        if(exp[i] == '-' || exp[i] == '+')
+            if(exp[i] == '-')
+                !checkPrevious(exp[i-1]) ? exp[i++] = '#':exp[i++];     //replace unary by '#'
+            else
+            {    
+                if(!checkPrevious(exp[i - 1])) 
+                    exp.erase(i,1); //remove '+' unary
+                i++;
+            }                            
+        else i++;                                                   //if it's not '+' or '-'
+    } 
 }
