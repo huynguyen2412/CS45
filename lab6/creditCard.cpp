11af362da435c01412c3ffb85ecde8b9a05ebadb
Huy Nguyen <huynguyen2412@gmail.com>
2018-12-07 22:14:10 -0800

    updated main

--- lab6/creditCard.cpp
+++ lab6/creditCard.cpp
@@ -2,7 +2,7 @@
   CS45 DISCRETE STRUCTURES WITH COMPUTER SCIENCE APPLICATIONS
   Professor PAUL WILKINSON
   Tom Nguyen
-  Test 2
+  Creditcard Generator
 */
 #include <iostream>
 #include <string.h>
@@ -11,51 +11,23 @@
 #include <time.h>       /* time */
 #include <ctime>
 #include <chrono>
+#include <vector>
 
 using namespace std;
 bool getInput(string &line);
 bool validateCredit();
 bool checkDigit(string creditNumber);
 char toChar(int num);
-void generatorCC(string& input);
+string ccHelper(string& input);
+void generatorCC(string storage[100000]);
 
 int main(int argc, char const *argv[])
 {
     string input;
     int total = 0;
     srand(time(NULL));
-    double average=0;
-    
-    // while(getInput(input))
-    // {
-    //     if(input.size()>16 && input.find_first_not_of("0123456789") < input.size())
-    //         cout <<"Invalid input."<<endl;
-    //     else if(checkDigit(input))
-    //         cout<<"Credit card is valid."<<endl;
-    //     else
-    //         cout<<"Credit card is invalid."<<endl;
-    // }
-    cout<<"STARTING --"<<endl;
-    while(total<5)
-    {
-        clock_t begin = clock();
-        // auto start = std::chrono::high_resolution_clock::now();
-        generatorCC(input);
-        if(checkDigit(input))
-        {
-            // cout<<input<<"--"<<endl;
-            total++;
-            clock_t end = clock();
-            double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-            // auto finish = std::chrono::high_resolution_clock::now();
-            // std::chrono::duration<double> elapsed = finish - start;
-            average+=elapsed_secs;
-        }
-
-        // double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    }
-    cout << "Total elapsed time: "<<average <<" secs"<<endl;
-    cout<<"ENDING --"<<endl;
+    string storage[100000];
+    generatorCC(storage);
     return 0;
 }
 
@@ -92,9 +64,25 @@ char toChar(int num)
 /*  Create a 16-digit number
     First digit is between 3-6 represent CreditCard provider
  */
-void generatorCC(string& input)
+string ccHelper(string& input)
 {
     input ="";
     for(int i=0;i<16;++i)
         (i==0)? input+=toChar((rand()%4+3)) : input+=toChar((rand()%10));
+    return input;
+}
+
+//check if digit valid add to the array
+void generatorCC(string storage[100000])
+{
+    int count =0;
+    while(count < 100000)
+    {
+        string temp="";
+        if(checkDigit(ccHelper(temp)))
+        {
+            storage[count] = (ccHelper(temp));
+            count++;
+        }
+    }
 }
